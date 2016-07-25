f30b76abfcb16ff73579d93c5f8d0fb98c650afa
dannyid <danielshekhtman@gmail.com>
2016-07-25 00:46:02 -0400

    Convert reducer to micogrcams

--- js/components/DailyRequirements.js
+++ js/components/DailyRequirements.js
@@ -5,23 +5,33 @@ import convert from 'convert-units';
 
 const calculateTotalNutrition = (foodItems) => {
   return foodItems
-    .reduce((
-      totalNutrition,
-      {
-        amount,
-        foodItem: {
-          nutrients
-        }
+  .reduce((
+    totalNutrition,
+    {
+      amount,
+      foodItem: {
+        nutrients
       }
-    ) => {
-      nutrients.forEach(({ nutr_no, nutrdesc, nutr_val, units }) => {
-        if (typeof totalNutrition[nutr_no] === 'undefined') {
-          totalNutrition[nutr_no] = 0;
-        }
-        totalNutrition[nutr_no] += (amount * parseInt(nutr_val, 10));
-      });
-      return totalNutrition;
-    }, {});
+    }
+  ) => {
+    nutrients
+    // Remove nutrients that aren't in some form of grams
+    // Like IUs for some vitamins
+    .filter(({ units }) => units.indexOf('g') > -1)
+    .forEach(({ nutr_no, nutrdesc, nutr_val, units }) => {
+      // Convert micrograms to something convert() understands
+      if (units === 'µg') units = 'mcg';
+      if (typeof totalNutrition[nutr_no] === 'undefined') {
+        totalNutrition[nutr_no] = 0;
+      }
+
+      const nutrValInMicrograms = convert(+nutr_val).from(units).to('mcg');
+      // Since nutrition data in the database is per 100 g, but we store all
+      // nutrient data in µg, we must divide the number of grams given by the user by 100
+      totalNutrition[nutr_no] = Math.floor((totalNutrition[nutr_no] + (amount / 100 * nutrValInMicrograms)) * 10) / 10;
+    });
+    return totalNutrition;
+  }, {});
 };
 
 const renderRequirements = (foodItems) => {
